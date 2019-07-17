f2e902cfebf66da8265cf760fbc6c7ecd2db9ba0
edgarc0998 <edgarc0998@gmail.com>
2019-07-17 10:46:43 -0500

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,23 @@
+function bubbleSort(array) {
+    var sorted = false;;
+
+while(!sorted) {
+    sorted = true;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i + 1] < array[i]) {
+    
+            var temp;
+            temp = array[i+1];
+            array[i+1] = array[i];
+            array[i] = temp;
+            sorted = false;
+        }
+      }
+}
+  return array;
+}
+
+
+console.log(bubbleSort([5, 4, 3, 1, 2]));
---
+++ bubblesort.spec.js
@@ -0,0 +1,11 @@
+
+describe('Bubble Sort', function(){
+    it('handles an empty array', function(){
+      expect( bubbleSort([]) ).toEqual( [] );
+    });
+
+
+
+
+  });
+
